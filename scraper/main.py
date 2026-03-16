8fc76d48a2e9096ea19e18db09730bffdaca1e41
shrub <shrub719@gmail.com>
2026-03-16 11:49:18 +0000

    add crumbs + img to parser

--- scraper/main.py
+++ scraper/main.py
@@ -27,22 +27,47 @@ def parse_page(href, f):
 
     res = requests.get(URL + href)
     soup = BeautifulSoup(res.content, "html.parser")
-    p = soup.select("div.entry-content p")
+
+    ps = soup.select("div.entry-content p")
+    imgs = soup.select("div.entry-content div.center-image img")
+    crumbs = soup.select("nav.breadcrumbs ul.breadcrumb li:first-child")
 
     id = href.split(".")[0][1:]
     title = soup.find("h1").text
-    if p: 
-        desc = str(p[0].encode_contents())
-        if len(desc) > MAX_DESC_LEN: desc = desc[:MAX_DESC_LEN]
+
+    desc = ""
+    desc_text = ""
+    if ps: 
+        inner = str(ps[0].encode_contents())
+        sentence_count = 0
+        for char in inner:
+            desc += char
+            if len(desc) >= 2 and desc[-2:] == ". ": sentence_count += 1
+            if sentence_count >= 2: break
+
+        text = ps[0].text
+        sentence_count = 0
+        for char in text:
+            desc_text += char
+            if len(desc_text) >= 2 and desc_text[-2:] == ". ": sentence_count += 1
+            if sentence_count >= 2: break
+
         desc = ( desc
             .replace('src="/', 'src="' + URL + '/')
             .replace('href="/', 'href="' + URL + '/')
             .replace("\n", "")
         )
-    else: 
-        desc = ""
+        desc_text = desc_text.replace("\n", "")
 
-    f.write(f'{{ id: "{id}", title: "{title}", desc: """{desc}""" }},\n')
+    img_src = ""
+    if imgs:
+        img_src = URL + imgs[0].get("src")
+
+    categories = []
+    if crumbs:
+        categories = list(set(crumb.text.replace("\n", "") for crumb in crumbs))
+
+    f.write(f'{{ id: "{id}", title: "{title}", desc: """{desc}""", descText: """{desc_text}""", img: "{img_src}", categories: {str(categories)} }},\n')
     f.flush()
 
 def parse_page_simple(href, text, f):
