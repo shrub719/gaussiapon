ba6ac7a332928f7f0dcd3f9fb1af2134cfcb5f61
shrub <shrub719@gmail.com>
2026-03-16 21:04:28 +0000

    add a fun animation + temp display random item!

--- site/script.js
+++ site/script.js
@@ -1,5 +1,32 @@
+const URL = "https://mathworld.wolfram.com";
+
 const machine = document.getElementById("machine");
+const result = document.getElementById("result");
+let pages = {};
+
+machine.ondragstart = () => false;
+
+// are you really gonna load that each time....
+fetch("assets/pages.json")
+    .then(response => response.json())
+    .then(data => { pages = data; })
+
+/*
+machine.addEventListener("mouseup", () => {
+    page = pages[Math.floor(Math.random() * pages.length)];
+    result.innerHTML = `<a href="${URL}/${page.id}.html" target="_blank">${page.title}</a>`;
+});
+*/
+
+function getPage() {
+    page = pages[Math.floor(Math.random() * pages.length)];
+    result.innerHTML = `<a href="${URL}/${page.id}.html" target="_blank">${page.title}</a>`;
+}
 
 machine.addEventListener("click", () => {
-    
+    machine.classList = ["clicked"];
+    setTimeout(() => {
+        machine.classList = [];
+        getPage();
+    }, 200);
 });
