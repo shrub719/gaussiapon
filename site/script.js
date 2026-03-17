ae663699407d959b1d81ca27f0677bc5b1b60eac
shrub <shrub719@gmail.com>
2026-03-17 21:16:00 +0000

    add explosion + ball animation!

--- site/script.js
+++ site/script.js
@@ -2,25 +2,24 @@ const URL = "https://mathworld.wolfram.com";
 
 const machine = document.getElementById("machine");
 const result = document.getElementById("result");
+const ball = document.getElementById("ball");
+const explosion = document.getElementById("explosion");
+
 let pages = {};
 
 machine.ondragstart = () => false;
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+let inProgress = false;
 
 // are you really gonna load that each time....
 fetch("assets/pages.json")
     .then(response => response.json())
     .then(data => { pages = data; })
 
-/*
-machine.addEventListener("mouseup", () => {
+async function getPage() {
     page = pages[Math.floor(Math.random() * pages.length)];
-    result.innerHTML = `<a href="${URL}/${page.id}.html" target="_blank">${page.title}</a>`;
-});
-*/
-
-function getPage() {
-    page = pages[Math.floor(Math.random() * pages.length)];
-    result.textContent = "";
 
     const title = document.createElement("a");
     title.innerText = page.title;
@@ -36,10 +35,35 @@ function getPage() {
     }
 }
 
-machine.addEventListener("click", () => {
-    machine.classList = ["clicked"];
-    setTimeout(() => {
-        machine.classList = [];
-        getPage();
-    }, 200);
-});
+async function machineClick() {
+    // TODO: check if can pull
+    if (inProgress) { return }
+
+    inProgress = true;
+
+    machine.classList = ["squish"];
+    ball.classList = ["move"];
+    result.textContent = "";
+
+    await delay(200);
+
+    machine.classList = [];
+
+    await delay(300);
+
+    ball.classList = [];
+    explosion.classList = ["explode"];
+    explosion.currentTime = 0;
+    explosion.play();
+
+    getPage();
+    
+    await delay(700);
+
+    explosion.classList = [];
+
+    inProgress = false;
+}
+
+machine.addEventListener("mousedown", machineClick);
+
